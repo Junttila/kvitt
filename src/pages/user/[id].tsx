a2c81522a71c7f05a48117d8cf95f14489aa8f65
Joakim Junttila <joakim@juntti.la>
2023-03-26 06:57:22 +0200

    Forgot new file

---
+++ src/pages/user/[id].tsx
@@ -0,0 +1,79 @@
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { api } from "~/utils/api";
+
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
+
+const ProfilePage: NextPage<{ userId: string }> = ({ userId }) => {
+  const { data } = api.profiles.getUserById.useQuery({
+    userId,
+  });
+  if (!data) return <div>404</div>;
+
+  return (
+    <>
+      <Head>
+        <title>{data.username}</title>
+      </Head>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile picture`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] 
+              ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+        <ProfileFeed userId={data.id}></ProfileFeed>
+      </PageLayout>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const userId = context.params?.id;
+
+  if (typeof userId !== "string") throw new Error("no slug");
+
+  await ssg.profiles.getUserById.prefetch({ userId });
+
+  return {
+    props: { trpcState: ssg.dehydrate(), userId },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
+
+export default ProfilePage;
